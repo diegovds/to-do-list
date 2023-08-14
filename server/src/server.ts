cc240f2f359c55522b7a259a3ba3d1d5a0a4d020
Diego Viana <dsantos@inf.ufsm.br>
2023-08-14 17:44:03 -0300

    feat: add eslint and fastify dependencies

---
+++ server/src/server.ts
@@ -0,0 +1,14 @@
+import fastfy from 'fastify'
+
+const app = fastfy()
+app.get('/hello', () => {
+  return 'Hello World'
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('🚀 HTTP server running on http://localhost:3333')
+  })
