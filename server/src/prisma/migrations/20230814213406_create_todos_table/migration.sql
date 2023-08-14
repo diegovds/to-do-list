c5acde663ca1736d66ad3aa29ef48ca0535136ff
Diego Viana <dsantos@inf.ufsm.br>
2023-08-14 18:36:28 -0300

    feat: add todos table

---
+++ server/src/prisma/migrations/20230814213406_create_todos_table/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "todos" (
+    "id" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "priority" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "todos_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "todos" ADD CONSTRAINT "todos_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
