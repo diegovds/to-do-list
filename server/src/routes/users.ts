788c7d3c0f64bfa990cb063b3375559c4c5c7711
Diego Viana <dsantos@inf.ufsm.br>
2023-08-14 22:08:17 -0300

    feat: add signin and signup users

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,10 +1,11 @@
 import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
+import { prisma } from '../lib/prisma'
 
 export async function usersRoute(app: FastifyInstance) {
-  app.post('/users', async (request) => {
+  app.post('/users', async (request, reply) => {
     const bodySchema = z.object({
-      name: z.string(),
+      name: z.string().optional(),
       email: z.string().email({ message: 'email não válido' }),
       password: z
         .string()
@@ -13,8 +14,47 @@ export async function usersRoute(app: FastifyInstance) {
 
     const { name, email, password } = bodySchema.parse(request.body)
 
-    const passwordHash = await app.bcrypt.hash(password)
+    let passwordVerify = false
+    let passwordHash = ''
 
-    return { name, email, passwordHash }
+    let user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (user) {
+      passwordVerify = await app.bcrypt.compare(password, user.password)
+    }
+
+    if (!user && name) {
+      passwordHash = await app.bcrypt.hash(password)
+
+      user = await prisma.user.create({
+        data: {
+          name,
+          email,
+          password: passwordHash,
+        },
+      })
+    }
+
+    if ((passwordVerify || passwordHash.length > 0) && user) {
+      const token = app.jwt.sign(
+        {
+          name: user.name,
+        },
+        {
+          sub: user.id,
+          expiresIn: '30 days',
+        },
+      )
+
+      return { token }
+    }
+
+    return reply
+      .status(400)
+      .send({ message: 'Ocorreu um erro ao fazer login ou ao criar usuário.' })
   })
 }
