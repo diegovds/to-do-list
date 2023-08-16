de4b3c1b9a6423f5dd380f4542700e414e23b8e2
Diego Viana <dsantos@inf.ufsm.br>
2023-08-16 15:04:36 -0300

    style: extending the styles in tailwind.config

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -5,7 +5,15 @@ export default {
       "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-      extend: {},
+      extend: {
+        backgroundColor: {
+          darker: '#2f3136',
+          card: '#202225'
+        },
+        boxShadow: {
+          card: 'rgb(204, 204, 204) 0px 0px 4px'
+        }
+      },
     },
     plugins: [],
   }
