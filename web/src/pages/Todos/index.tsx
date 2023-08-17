61bfcc9851a5df5f640aec02bd7e0a358ebf1fa0
Diego Viana <dsantos@inf.ufsm.br>
2023-08-17 20:55:50 -0300

    feat: add logout button in todos page

--- web/src/pages/Todos/index.tsx
+++ web/src/pages/Todos/index.tsx
@@ -1,12 +1,35 @@
+import Cookies from "js-cookie";
 import { useContext } from "react";
+import Button from "../../components/Button";
 import { Context } from "../../contexts/Context";
+import { UserActions } from "../../types/reducerActionType";
 
 const Todos = () => {
-  const { state } = useContext(Context);
+  const { state, dispatch } = useContext(Context);
+
+  const handleLogout = () => {
+    Cookies.remove("token");
+
+    dispatch({
+      type: UserActions.setName,
+      payload: {
+        name: "",
+      },
+    });
+
+    dispatch({
+      type: UserActions.setToken,
+      payload: {
+        token: "",
+      },
+    });
+  };
+
   return (
     <div>
       <h1>Todos - Olá {state.user.name}</h1>
       <p>{state.user.token}</p>
+      <Button onClick={handleLogout}>Clique para sair</Button>
     </div>
   );
 };
