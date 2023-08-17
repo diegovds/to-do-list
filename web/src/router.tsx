635994ec12bff36358ae4c8612acc5c47f295c28
Diego Viana <dsantos@inf.ufsm.br>
2023-08-17 15:43:58 -0300

    Revert "chore: move BrowserRouter to main.tsx"
    This reverts commit 878e1de88e108e83da65623198fdcb3bd3f84bb6.

--- web/src/router.tsx
+++ web/src/router.tsx
@@ -1,17 +1,19 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Todos from "./pages/Todos";
 
 const Router = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Signin />} />
-      <Route path="/signin" element={<Signin />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/todos" element={<Todos />} />
-      <Route path="*" element={<Signin />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Signin />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/todos" element={<Todos />} />
+        <Route path="*" element={<Signin />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
