b17b67c7b3dfcd766006461b02b2f56672070153
Diego Viana <dsantos@inf.ufsm.br>
2023-08-17 20:55:03 -0300

    feat: add verification of authenticated routes

--- web/src/router.tsx
+++ web/src/router.tsx
@@ -1,17 +1,30 @@
+import { useContext } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Context } from "./contexts/Context";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Todos from "./pages/Todos";
 
 const Router = () => {
+  const { state } = useContext(Context);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Signin />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/todos" element={<Todos />} />
-        <Route path="*" element={<Signin />} />
+        <Route path="/" element={!state.user.token ? <Signin /> : <Todos />} />
+        <Route
+          path="/signin"
+          element={!state.user.token ? <Signin /> : <Todos />}
+        />
+        <Route
+          path="/signup"
+          element={!state.user.token ? <Signup /> : <Todos />}
+        />
+        <Route
+          path="/todos"
+          element={!state.user.token ? <Signin /> : <Todos />}
+        />
+        <Route path="*" element={!state.user.token ? <Signin /> : <Todos />} />
       </Routes>
     </BrowserRouter>
   );
