8231ebd88fc1300b0dd869e2a21b9bcfb27f102e
Diego Viana <dsantos@inf.ufsm.br>
2023-08-16 22:04:16 -0300

    feat: add validation to the sign in form

--- web/src/components/Input.tsx
+++ web/src/components/Input.tsx
@@ -1,20 +1,21 @@
-import { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, RefObject, forwardRef } from "react";
 import { cn } from "../libs/utils";
 
 type InputProps = InputHTMLAttributes<HTMLInputElement>;
 
-const Input = ({ children, className, ...props }: InputProps) => {
+const Input = forwardRef(({ children, className, ...props }: InputProps, ref) => {
   return (
     <input
       className={cn(
         "focus: h-14 w-full rounded-lg bg-darker p-4 text-gray-50 outline-none ring-darker placeholder:text-gray-400 focus:ring-2",
         className
       )}
+      ref={ref as RefObject<HTMLInputElement>}
       {...props}
     >
       {children}
     </input>
   );
-};
+});
 
 export default Input;
--- web/src/pages/Signin/index.tsx
+++ web/src/pages/Signin/index.tsx
@@ -1,23 +1,55 @@
-import { FormEvent } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import Button from "../../components/Button";
-
 import Form from "../../components/Form";
 import FormHeader from "../../components/Form/FormHeader";
 import Input from "../../components/Input";
 import Link from "../../components/Link";
 
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+type FormData = z.infer<typeof formSchema>;
+
 const Signin = () => {
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const {
+    handleSubmit,
+    register,
+    formState: { isSubmitting },
+  } = useForm<FormData>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const { ref: emailRef } = register("email");
+  const { ref: passwordRef } = register("password");
+
+  const onSubmit = async (data: FormData) => {
+    console.log(data);
   };
 
   return (
     <section className="flex min-h-screen justify-center items-center">
-      <Form onSubmit={handleSubmit}>
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <FormHeader title="Entrar" description="Insira as suas credenciais" />
-        <Input type="email" placeholder="E-mail" />
-        <Input type="password" placeholder="Senha" />
-        <Button className="w-full my-3">Login</Button>
+        <Input
+          type="text"
+          placeholder="E-mail"
+          autoFocus
+          {...register("email", { required: true })}
+          ref={emailRef}
+        />
+        <Input
+          type="password"
+          placeholder="Senha"
+          {...register("password", { required: true })}
+          ref={passwordRef}
+        />
+        <Button className="w-full my-3" disabled={isSubmitting}>
+          Login
+        </Button>
         <div className="flex flex-col items-center xsm:flex-row gap-1">
           <p className="text-sm">Ainda não possui uma conta?</p>
           <Link className="underline text-sm" to={`/signup`}>
