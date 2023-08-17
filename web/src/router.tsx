878e1de88e108e83da65623198fdcb3bd3f84bb6
Diego Viana <dsantos@inf.ufsm.br>
2023-08-17 15:31:12 -0300

    chore: move BrowserRouter to main.tsx

--- web/src/router.tsx
+++ web/src/router.tsx
@@ -1,19 +1,17 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Signin from "./pages/Signin";
 import Signup from "./pages/Signup";
 import Todos from "./pages/Todos";
 
 const Router = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Signin />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/todos" element={<Todos />} />
-        <Route path="*" element={<Signin />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Signin />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/todos" element={<Todos />} />
+      <Route path="*" element={<Signin />} />
+    </Routes>
   );
 };
 
