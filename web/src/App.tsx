78377200ae424dbeab343f9e35558ad45e0177c4
Diego Viana <dsantos@inf.ufsm.br>
2023-08-17 20:57:30 -0300

    feat: checking for existence of cookie in app file

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,43 @@
+import Cookies from "js-cookie";
+import decode from "jwt-decode";
+import { useContext, useEffect, useState } from "react";
+import { Context } from "./contexts/Context";
 import Router from "./router";
+import { UserActions } from "./types/reducerActionType";
+import { User } from "./types/user";
 
 const App = () => {
+  const [cookieVerification, setCookieVerification] = useState(false);
+  const { dispatch } = useContext(Context);
+
+  useEffect(() => {
+    const token = Cookies.get("token");
+
+    if (token) {
+      const user: User = decode(token);
+
+      dispatch({
+        type: UserActions.setName,
+        payload: {
+          name: user.name,
+        },
+      });
+
+      dispatch({
+        type: UserActions.setToken,
+        payload: {
+          token,
+        },
+      });
+    }
+
+    setCookieVerification(true);
+  }, [dispatch]);
+
+  if (!cookieVerification) {
+    return <div />;
+  }
+
   return <Router />;
 };
 
