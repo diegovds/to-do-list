c047f8dcee85c358ad24a9f8619e928583584280
Diego Viana <dsantos@inf.ufsm.br>
2023-08-18 23:59:11 -0300

    refactor: new implementation of component SectionHeader

--- web/src/components/SectionHeader.tsx
+++ web/src/components/SectionHeader.tsx
@@ -1,13 +1,23 @@
-type SectionHeaderProps = {
+import { HTMLAttributes } from "react";
+import { cn } from "../libs/utils";
+
+type SectionHeaderProps = HTMLAttributes<HTMLDivElement> & {
   title: string;
   description: string;
 };
 
-const SectionHeader = ({ title, description }: SectionHeaderProps) => {
+const SectionHeader = ({
+  children,
+  className,
+  title,
+  description,
+  ...props
+}: SectionHeaderProps) => {
   return (
-    <div className="flex flex-col items-center">
+    <div className={cn("flex flex-col items-center", className)} {...props}>
       <h1 className="text-4xl mb-3">{title}</h1>
       <p className="text-base text-gray-300">{description}</p>
+      {children}
     </div>
   );
 };
