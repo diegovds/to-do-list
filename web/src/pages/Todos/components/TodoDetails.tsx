86921ef46abdcb2f3761a7c95d92549bfefb194b
Diego Viana <dsantos@inf.ufsm.br>
2023-08-21 17:39:33 -0300

    feat: add TodoDetails component

---
+++ web/src/pages/Todos/components/TodoDetails.tsx
@@ -0,0 +1,32 @@
+import { cn } from "../../../libs/utils";
+import { Todo } from "../../../types/Todos";
+
+type TodoDetailsProps = {
+  todo: Todo;
+  deleteTodo: (id: string) => void;
+};
+
+const TodoDetails = ({
+  todo: { id, content, priority },
+  deleteTodo,
+}: TodoDetailsProps) => {
+  return (
+    <div
+      className={cn(
+        `flex justify-between p-5`,
+        priority === "high"
+          ? `bg-red-500`
+          : priority === "medium"
+          ? `bg-yellow-400`
+          : `bg-green-500`
+      )}
+    >
+      <h1>{content}</h1>
+      <button className="bg-black rounded" onClick={() => deleteTodo(id)}>
+        Deletar
+      </button>
+    </div>
+  );
+};
+
+export default TodoDetails;
