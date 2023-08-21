cc4bde871f72b0cbe28b64c41e6f341c136072ae
Diego Viana <dsantos@inf.ufsm.br>
2023-08-21 15:33:03 -0300

    feat: add todos type

---
+++ web/src/types/Todos.ts
@@ -0,0 +1,13 @@
+type Todo = {
+    id: string,
+    content: string,
+    priority: string,
+    status: string,
+    createdAt: string,
+    updatedAt: string,
+    userId: string
+}
+
+export type Todos = {
+    todos: Todo[]
+}
