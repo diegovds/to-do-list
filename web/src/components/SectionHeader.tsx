1ebdfccd4f95b306bc2f6fe432442c3d2aaa94bf
Diego Viana <dsantos@inf.ufsm.br>
2023-09-13 20:23:20 -0300

    style: limiting text in SectionHeader component

--- web/src/components/SectionHeader.tsx
+++ web/src/components/SectionHeader.tsx
@@ -15,7 +15,7 @@ const SectionHeader = ({
 }: SectionHeaderProps) => {
   return (
     <div className={cn("flex flex-col items-center", className)} {...props}>
-      <h1 className="text-2xl tracking-wider font-bold mb-3">{title}</h1>
+      <h1 className="text-2xl tracking-wider font-bold mb-3 line-clamp-1">{title}</h1>
       <p className="text-sm tracking-wider text-gray-300">{description}</p>
       {children}
     </div>
